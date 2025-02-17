2de5943d1d95b0b15dfc8eac15e249f190bf5792
SirMeows <sirmeowsh@gmail.com>
2025-02-17 15:33:35 +0100

    comment out incomplete test

--- src/app/review-add/review-add.component.spec.ts
+++ src/app/review-add/review-add.component.spec.ts
@@ -1,34 +1,34 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { ReviewAddComponent } from './review-add.component';
-import { testingServices } from '../../test';
-import { HttpTestingController, TestRequest } from '@angular/common/http/testing';
-import { Review } from '../../models/review.model';
-
-describe('onSubmit', () => {
-  let component: ReviewAddComponent;
-  let fixture: ComponentFixture<ReviewAddComponent>;
-  let httpTesting: HttpTestingController;
-  let request: TestRequest;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [ReviewAddComponent], providers: testingServices
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(ReviewAddComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-   
-    // for mocking api response
-    httpTesting = TestBed.inject(HttpTestingController);
-
-  });
-
-  it('should be able to store reviewForm value in variable of type Review', ()=>{
-      
-
-    expect( component.reviewForm.value).toBeInstanceOf(Review);
-  });
-
-});
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
+// import { ReviewAddComponent } from './review-add.component';
+// import { testingServices } from '../../test';
+// import { HttpTestingController, TestRequest } from '@angular/common/http/testing';
+// import { Review } from '../../models/review.model';
+//
+// describe('onSubmit', () => {
+//   let component: ReviewAddComponent;
+//   let fixture: ComponentFixture<ReviewAddComponent>;
+//   let httpTesting: HttpTestingController;
+//   let request: TestRequest;
+//
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       imports: [ReviewAddComponent], providers: testingServices
+//     })
+//     .compileComponents();
+//
+//     fixture = TestBed.createComponent(ReviewAddComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//
+//     // for mocking api response
+//     httpTesting = TestBed.inject(HttpTestingController);
+//
+//   });
+//
+//   it('should be able to store reviewForm value in variable of type Review', ()=>{
+//
+//
+//     expect( component.reviewForm.value).toBeInstanceOf(Review);
+//   });
+//
+// });
